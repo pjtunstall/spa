91f78357ef45a2797315fb722104620dab8e8049
pjunstall <pjtunstall@gmail.com>
2024-07-12 00:01:47 +0100

    reverted after failed attempt to test other features

--- main_test.go
+++ main_test.go
@@ -1,57 +1,46 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 // This test does not require an internet connection to run. The test server is created using the httptest.NewServer function, which creates a test server running on a random port on the local machine. The test server is closed when the test function exits, so it does not rely on an external server.
 
 func TestMain(t *testing.T) {
-    // Create a test server with the mainHandler
-    ts := httptest.NewServer(http.HandlerFunc(mainHandler))
-    defer ts.Close()
+	// Test that static files are served correctly
+	t.Run("StaticFileServed", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/style.css", nil)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(mainHandler)
+		handler.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+	})
 
-    // Test case for serving static files
-    t.Run("StaticFileServed", func(t *testing.T) {
-        // Create a request to the static file
-        req, err := http.NewRequest("GET", ts.URL+"/style.css", nil)
-        if err != nil {
-            t.Fatal(err)
-        }
+	// Test that index.html is served for non-static paths
+	t.Run("IndexServed", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/", nil)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(mainHandler)
+		handler.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+	})
 
-        // Send the request to the test server
-        resp, err := http.DefaultClient.Do(req)
-        if err != nil {
-            t.Fatal(err)
-        }
-        defer resp.Body.Close()
-
-        // Check the response status code
-        if resp.StatusCode != http.StatusOK {
-            t.Errorf("handler returned wrong status code: got %v want %v", resp.StatusCode, http.StatusOK)
-        }
-    })
-
-    // Test case for serving the index.html file
-    t.Run("IndexFileServed", func(t *testing.T) {
-        // Create a request to the index.html file
-        req, err := http.NewRequest("GET", ts.URL+"/", nil)
-        if err != nil {
-            t.Fatal(err)
-        }
-
-        // Send the request to the test server
-        resp, err := http.DefaultClient.Do(req)
-        if err != nil {
-            t.Fatal(err)
-        }
-        defer resp.Body.Close()
-
-        // Check the response status code
-        if resp.StatusCode != http.StatusOK {
-            t.Errorf("handler returned wrong status code: got %v want %v", resp.StatusCode, http.StatusOK)
-        }
-    })
+	// Test that the server starts without error
+	t.Run("ServerStarts", func(t *testing.T) {
+		go func() {
+			if err := http.ListenAndServe(":3000", nil); err != nil {
+				log.Fatal("Error starting server: ", err)
+			}
+		}()
+		// Give the server some time to start
+		time.Sleep(time.Second)
+	})
 }
